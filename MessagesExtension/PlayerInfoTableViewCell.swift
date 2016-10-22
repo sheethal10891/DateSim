227f1aee0e359d6b57bdcff4f3640034491785ef
avinashsastry <avinash.mail@gmail.com>
2016-10-22 16:39:42 +0530

    Added fields to player cell

--- MessagesExtension/PlayerInfoTableViewCell.swift
+++ MessagesExtension/PlayerInfoTableViewCell.swift
@@ -10,8 +10,18 @@ import UIKit
 
 class PlayerInfoTableViewCell: UITableViewCell {
     
-    
+    // Text field to show the player's name
     @IBOutlet weak var playerNameLabel: UILabel!
     
+    // This is the invite button
     @IBOutlet weak var TestBtn: UIButton!
+    
+    @IBOutlet weak var heartsIcon: UIImageView!
+    
+    @IBOutlet weak var heartCount: UILabel!
+    @IBOutlet weak var busyIcon: UIImageView!
+    @IBOutlet weak var pendingText: UILabel!
+    @IBOutlet weak var challengeBtn: UIButton!
+    @IBOutlet weak var ignoreBtn: UIButton!
+    @IBOutlet weak var delicnedOrBusyText: UILabel!
 }
